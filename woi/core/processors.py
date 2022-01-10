57cd55d2803df06e770a566e85d6f4214f898fd6
Whitie <weimann.th@gmail.com>
2022-01-10 13:11:56 +0100

    Changed markdown preview function (now timeout based).

--- woi/core/processors.py
+++ woi/core/processors.py
@@ -22,6 +22,9 @@ def add_woi_variables(req):
         'woi_subtitle': getattr(settings, 'WOI_SUBTITLE', ''),
         'woi_footer': getattr(settings, 'WOI_FOOTER', True),
         'woi_content_license': settings.WOI_CONTENT_LICENSE,
+        'woi_preview_timeout_ms': getattr(
+            settings, 'WOI_PREVIEW_TIMEOUT_MS', 1000
+        ),
         'woi_ext_js': bool(q.count()),
         'woi_ip': req.META.get('REMOTE_ADDR', ''),
         'woi_js': woi_js,

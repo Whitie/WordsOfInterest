5bde5c184026743db47107d9c08e9b54b0a1ef2f
Whitie <weimann.th@gmail.com>
2022-01-10 13:34:08 +0100

    Added dark theme for highlight.js.

--- woi/core/processors.py
+++ woi/core/processors.py
@@ -6,6 +6,11 @@ from .models import Extension
 
 def add_woi_variables(req):
     q = Extension.objects.filter(active=True).exclude(js_file__in=['', None])
+    theme = getattr(settings, 'WOI_HLJS_THEME', 'light')
+    if theme == 'dark':
+        hljs_theme = '3rd/css/agate.min.css'
+    else:
+        hljs_theme = '3rd/css/github.min.css'
     woi_js = {
         'left_url': req.build_absolute_uri(
             reverse('core:extensions', args=('left',))
@@ -25,6 +30,7 @@ def add_woi_variables(req):
         'woi_preview_timeout_ms': getattr(
             settings, 'WOI_PREVIEW_TIMEOUT_MS', 1000
         ),
+        'woi_hljs_theme': hljs_theme,
         'woi_ext_js': bool(q.count()),
         'woi_ip': req.META.get('REMOTE_ADDR', ''),
         'woi_js': woi_js,
